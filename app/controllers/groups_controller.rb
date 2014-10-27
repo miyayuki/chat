5e3b554adc027bdebf8770c32f0b5e611ebe0c30
melonsoda5ml <mitsuba@sfc.wide.ad.jp>
2014-10-27 23:53:17 +0900

    edit show action in group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,28 +3,27 @@ class GroupsController < ApplicationController
 	def create
     p params
 
-    @group = Group.new
-    @group.name = params[:group][:name]
-    @group.save
-    p @group
+		@group = Group.new
+		@group.name = params[:group][:name]
+		@group.save
 
-    @group_subscription = GroupSubscription.new
-    @group_subscription.groupID = @group.id
-    @group_subscription.userID = current_user.id
-    @group_subscription.save
+		@group_subscription = GroupSubscription.new
+		@group_subscription.groupID = @group.id
+		@group_subscription.userID = current_user.id
+		@group_subscription.save
 
-    for id in params[:group_users] do
-      p id
-      @group_subscription = GroupSubscription.new
-      @group_subscription.groupID = @group.id
-      @group_subscription.userID = id.to_i
-      @group_subscription.save
-    end
+		for id in params[:group_users] do
+			p id
+			@group_subscription = GroupSubscription.new
+			@group_subscription.groupID = @group.id
+			@group_subscription.userID = id.to_i
+			@group_subscription.save
+		end
 
-    @group_master = GroupMaster.new
-    @group_master.groupID = @group.id
-    @group_master.userID = current_user.id
-    @group_master.save
+		@group_master = GroupMaster.new
+		@group_master.groupID = @group.id
+		@group_master.userID = current_user.id
+		@group_master.save
 
 	end
 
